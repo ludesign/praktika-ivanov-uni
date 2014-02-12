5f35c1fe40cb5558345501bca9527e2a62a78717
Zlobcho <ludesign.bg@gmail.com>
2014-02-12 15:22:57 +0200

    added message loop
    basic message handling loop with option for detecting GetMessage errors

--- zadacha1-sem5-win32/zadacha1-sem5-win32/zadacha1-sem5-win32.cpp
+++ zadacha1-sem5-win32/zadacha1-sem5-win32/zadacha1-sem5-win32.cpp
@@ -11,5 +11,17 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(pCmdLine);
 
-	return EXIT_SUCCESS;
+	// handle messages loop
+	BOOL bMsgFlag = 0;
+	MSG pMsg = { 0 };
+	while ((bMsgFlag = ::GetMessageW(&pMsg, NULL, 0, 0)) != 0) {
+		if (bMsgFlag > 0) { // GetMessageW returned no error nor processed WM_QUIT message
+			::TranslateMessage(&pMsg);
+			::DispatchMessageW(&pMsg);
+		} else { // handle error returned by GetMessageW
+			// TODO: add a helper function to use GetLastError with FormatMessage for retrieve the error string
+		}
+	}
+
+	return static_cast<int>(pMsg.wParam);
 }
